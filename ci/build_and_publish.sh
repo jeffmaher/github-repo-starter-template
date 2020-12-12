ac4751b7d7d4a61972f26738e59d9715157f5f20
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2020-12-12 18:35:47 -0500

    Add build and publish script

---
+++ ci/build_and_publish.sh
@@ -0,0 +1,7 @@
+# Fail if any command exits with a non-zero exit code
+set -e 
+
+# -----
+# REPLACE THE BELOW WITH YOUR COMMANDS
+# -----
+echo 'Build your artifacts and publish them (such as building your Docker container or packaging, then pushing to container repo or central package store)'
