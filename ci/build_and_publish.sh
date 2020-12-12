df47d072fa3f342285d5f256b705fcaad7f015ea
Jeff Maher <950486+jeffmaher@users.noreply.github.com>
2020-12-12 18:39:56 -0500

    Test build and publish

--- ci/build_and_publish.sh
+++
@@ -1,7 +0,0 @@
-# Fail if any command exits with a non-zero exit code
-set -e 
-
-# -----
-# REPLACE THE BELOW WITH YOUR COMMANDS
-# -----
-echo 'Build your artifacts and publish them (such as building your Docker container or packaging, then pushing to container repo or central package store)'
